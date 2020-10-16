4f24a6c970966cfc9a292304de435eb490fc40ef
Qi Wang <qi@broadinstitute.org>
2020-10-16 11:52:25 -0400

    use yq

--- .github/scripts/bumpLeonardoCronJobHash.sh
+++ .github/scripts/bumpLeonardoCronJobHash.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-TARGET_TAG=$1
+TARGET_JOB=$1
 
 git config user.email "broadbot@broadinstitute.org"
 git config user.name "broadbot"
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 
-sed -i "s/$TARGET_TAG: .*/$TARGET_TAG: $git_hash/g" charts/leonardo/values.yaml
-git commit -am "bump $TARGET_TAG version"
+yq w -i  charts/leonardo/values.yaml $TARGET_JOB.imageTag $git_hash
+git commit -am "bump $TARGET_JOB version"
