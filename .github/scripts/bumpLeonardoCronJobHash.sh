b74d05436b52879cec87de5750e4a815943cb5f3
broadbot <broadbot@broadinstitute.org>
2020-10-16 13:12:26 -0400

    add action

--- .github/scripts/bumpLeonardoCronJobHash.sh
+++ .github/scripts/bumpLeonardoCronJobHash.sh
@@ -2,8 +2,6 @@
 
 TARGET_JOB=$1
 
-git config user.email "broadbot@broadinstitute.org"
-git config user.name "broadbot"
 git_hash=$(git rev-parse --short "$GITHUB_SHA")
 
 yq w -i  charts/leonardo/values.yaml $TARGET_JOB.imageTag $git_hash
